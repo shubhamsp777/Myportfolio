b50e823b36d07716a16efe8accc03db3407fb403
Shubham Rajendra Pahune <59164300+shubhamsp777@users.noreply.github.com>
2023-07-22 18:11:21 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+  
+   var typed;
+
+   var typed = new Typed (".typing", {
+        strings: ["Student", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+   
+    var typed;
+   
+    var typed = new Typed (".typing-2", {
+        strings: ["Student", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+   
